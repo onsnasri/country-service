8d80a836e8e7e27ff487811c9cfad8a3a10d1d9e
Ons Nasri <nasrions33@gmail.com>
2025-10-26 20:45:20 +0100

    test phase

---
+++ src/main/java/com/countryservice/demo/beans/Country.java
@@ -0,0 +1,48 @@
+package com.countryservice.demo.beans;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor
+@Table(name="country")
+public class Country {
+	
+	@Id
+	@Column(name="id")
+	private int idCountry;
+	@Column(name="name_country")
+	private String name;
+	@Column(name="capital_name")
+	private String Capital;
+	public Country(int i, String string, String string2) {
+		// TODO Auto-generated constructor stub
+	}
+	public Country() {
+    }
+	public int getIdCountry() {
+		return idCountry;
+	}
+	public void setIdCountry(int idCountry) {
+		this.idCountry = idCountry;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCapital() {
+		return Capital;
+	}
+	public void setCapital(String capital) {
+		Capital = capital;
+	}
+
+}
